1126904f93ba7b15ceac295e7b7b1659ae9e5613
iminina <marveremenko@gmail.com>
2015-08-29 13:17:51 +0300

    i can download files!)))

---
+++ main.py
@@ -0,0 +1,25 @@
+__author__ = 'm_veremenko'
+
+import requests
+import json
+
+payload = {'auth_token': 'W5hwWhkx24BVYnQPxMyZ'}
+user_url = "https://api.desktoppr.co/1/users/iminina"
+
+wallpapers_url = "https://api.desktoppr.co/1/users/iminina/wallpapers/random"
+
+r1 = requests.get(wallpapers_url)
+json_response = r1.json()
+print json_response
+
+image_url = json_response[u'response'][u'image'][u'url']
+print image_url
+
+r2 = requests.get(image_url)
+
+if r2.status_code == 200:
+    f = open("D:/My/repo/wallpaper/sample.jpg", 'wb')
+    f.write(r2.content)
+    f.close()
+
+
