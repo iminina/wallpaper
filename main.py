3a0e5dcbf320a1623b285376df0829e0b1e9dcd6
iminina <marveremenko@gmail.com>
2015-09-02 17:51:58 +0300

    set wallpaper every 3 hour

--- main.py
+++ main.py
@@ -2,26 +2,35 @@ __author__ = 'm_veremenko'
 
 import requests
 import ctypes
+import threading
+import time
 
-payload = {'auth_token': 'W5hwWhkx24BVYnQPxMyZ'}
-user_url = "https://api.desktoppr.co/1/users/iminina"
-
-wallpapers_url = "https://api.desktoppr.co/1/users/iminina/wallpapers/random"
-
-r1 = requests.get(wallpapers_url)
-json_response = r1.json()
-print json_response
-
-image_url = json_response[u'response'][u'image'][u'url']
-print image_url
-
-r2 = requests.get(image_url)
-
-if r2.status_code == 200:
-    f = open("D:/My/repo/wallpaper/sample.jpg", 'wb')
-    f.write(r2.content)
-    f.close()
+# payload = {'auth_token': 'W5hwWhkx24BVYnQPxMyZ'}
+# user_url = "https://api.desktoppr.co/1/users/iminina"
 
 SPI_SETDESKWALLPAPER = 20
-ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, "D:/My/repo/wallpaper/sample.jpg", 0)
+wallpapers_url = "https://api.desktoppr.co/1/wallpapers/random"
 
+def set_wallpaper():
+    r1 = requests.get(wallpapers_url)
+    json_response = r1.json()
+    print json_response
+
+    image_url = json_response[u'response'][u'image'][u'url']
+    print image_url
+
+    r2 = requests.get(image_url)
+
+    if r2.status_code == 200:
+        f = open("sample.jpg", 'wb')
+        f.write(r2.content)
+        f.close()
+    ctypes.windll.user32.SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, "sample.jpg", 0)
+    return
+
+def run():
+  threading.Timer(10800.0, run).start()
+  set_wallpaper()
+  print time.strftime('%X %x %Z')
+
+run()
